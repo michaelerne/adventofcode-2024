e5364eb06aaf479a4071e96d9907bde3d33ec608
Michael Erne <michael.erne@gmail.com>
2024-12-07 07:44:46 +0100

    day 07 refactor

--- day_07.py
+++ day_07.py
@@ -12,43 +12,29 @@ def parse_data(data):
 
 
 def is_valid(numbers, expected_result, part_b=False):
-    memo = {}
-
     def backtrack(index, current_result):
-        if index == len(numbers):
-            return current_result == expected_result
+        if index < 0:
+            return current_result == 0
 
-        key = (index, current_result)
-        if key in memo:
-            return memo[key]
+        last_num = numbers[index]
 
-        next_num = numbers[index]
-
-        if current_result > expected_result:
-            memo[key] = False
-            return False
-
-        operations = ['+', '*']
         if part_b:
-            operations.append('|')
-
-        for op in operations:
-            if op == '+':
-                if backtrack(index + 1, current_result + next_num):
-                    return True
-            elif op == '*':
-                if backtrack(index + 1, current_result * next_num):
-                    return True
-            elif op == '|' and part_b:
-                # Arithmetic concatenation for '|'
-                concatenated = current_result * (10 ** len(str(next_num))) + next_num
-                if backtrack(index + 1, concatenated):
+            str_result = str(current_result)
+            str_last_num = str(last_num)
+            if str_result.endswith(str_last_num):
+                truncated_result = str_result[:-len(str_last_num)]
+                if truncated_result.isdigit() and backtrack(index - 1, int(truncated_result)):
                     return True
 
-        memo[key] = False
+        if backtrack(index - 1, current_result - last_num):
+            return True
+
+        if current_result % last_num == 0 and backtrack(index - 1, current_result // last_num):
+            return True
+
         return False
 
-    return backtrack(1, numbers[0])
+    return backtrack(len(numbers) - 1, expected_result)
 
 
 def part_a(data):
