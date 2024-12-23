9e1a0ef1d3bca2de06bef04da596064e08fad3ca
Michael Erne <michael.erne@gmail.com>
2024-12-23 07:26:05 +0100

    day 23 - refactor

--- day_23.py
+++ day_23.py
@@ -1,26 +1,61 @@
-import networkx as nx
+from collections import defaultdict
 
 from run_util import run_puzzle
 
 
 def parse_data(data):
-    return nx.Graph([x.split('-') for x in data.strip().split('\n')])
+    adjacency = defaultdict(set)
+    for line in data.strip().split('\n'):
+        left, right = line.split('-')
+        adjacency[left].add(right)
+        adjacency[right].add(left)
+    return adjacency
 
 
 def part_a(data):
-    graph = parse_data(data)
-
-    return sum(
-        1
-        for clique in nx.enumerate_all_cliques(graph)
-        if len(clique) == 3 and any(node.startswith('t') for node in clique)
-    )
+    adjacency = parse_data(data)
+    found = set()
+    for u in adjacency:
+        if u.startswith('t'):
+            neighbours = list(adjacency[u])
+            for i in range(len(neighbours) - 1):
+                for j in range(i + 1, len(neighbours)):
+                    v, w = neighbours[i], neighbours[j]
+                    if w in adjacency[v]:
+                        found.add(tuple(sorted([u, v, w])))
+    return len(found)
 
 
 def part_b(data):
-    graph = parse_data(data)
+    adjacency = parse_data(data)
+    best_clique, best_size = set(), 0
 
-    return ','.join(sorted(max(nx.find_cliques(graph), key=len)))
+    def bron_kerbosch(current_clique, candidates, excluded):
+        nonlocal best_clique, best_size
+        if not candidates and not excluded:
+            if len(current_clique) > best_size:
+                best_size = len(current_clique)
+                best_clique = current_clique
+            return
+        if len(current_clique) + len(candidates) <= best_size:
+            return
+        if candidates:
+            pivot, minimum_size = None, float('inf')
+            for candidate in candidates:
+                size_diff = len(candidates - adjacency[candidate])
+                if size_diff < minimum_size:
+                    minimum_size = size_diff
+                    pivot = candidate
+            for vertex in list(candidates - adjacency[pivot]):
+                new_clique = current_clique | {vertex}
+                new_candidates = candidates & adjacency[vertex]
+                new_excluded = excluded & adjacency[vertex]
+                bron_kerbosch(new_clique, new_candidates, new_excluded)
+                candidates.remove(vertex)
+                excluded.add(vertex)
+
+    bron_kerbosch(set(), set(adjacency.keys()), set())
+    return ",".join(sorted(best_clique))
 
 
 def main():
